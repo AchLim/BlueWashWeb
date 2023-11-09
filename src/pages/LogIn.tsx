7758cee03b20fc71e27088eeaf1276be80e8af57
AchLim <rimuakoru@gmail.com>
2023-11-10 03:08:45 +0700

    [UPDATE] Usage of useAuth

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,112 +1,120 @@
 import { useState } from 'react';
 import { Lock } from "@mui/icons-material";
 import {
-  Alert,
-  Avatar,
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  FormControlLabel,
-  TextField,
-  Typography,
+	Alert,
+	Avatar,
+	Box,
+	Button,
+	Checkbox,
+	Container,
+	FormControlLabel,
+	TextField,
+	Typography,
 } from "@mui/material";
-import { SignIn } from "../axios";
-import { useNavigate } from 'react-router-dom';
+import axios from "../axios";
+import { useLocation, useNavigate } from 'react-router-dom';
+import ILocationState from '../components/models/ILocationState';
+import useAuth from '../hooks/useAuth';
 
-interface ILoginProp {
-  setToken: React.Dispatch<React.SetStateAction<string | null>>
-}
 
-const LogIn = ({setToken}: ILoginProp) => {
-  const [error, setError] = useState(false);
-  const navigate = useNavigate();
+const Login = () => {
+	const { setAuth } = useAuth();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
+	const navigate = useNavigate();
+	const location = useLocation() as ILocationState;
+	const from = location.state?.from?.pathname || "/";
 
-    let login = data.get('login')?.toString();
-    let password = data.get('password')?.toString();
+	const [error, setError] = useState(false);
 
-    const fetchUser = async () => {
-        var response = await SignIn({login, password});
-        if (response.status == 200) {
-          var data = response.data;
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
 
-          localStorage.setItem('token', data);
-          setToken(data);
-          navigate('/');
-        }
-    };
+		let login = data.get('login')?.toString();
+		let password = data.get('password')?.toString();
 
-    fetchUser().catch(() => {
-      setError(true);
-    });
-  };
+		const fetchUser = async () => {
+			var response = await axios.post('/auth/login', { login, password });
+			if (response.status == 200) {
+				var data = response.data;
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          paddingInline: 5,
-          paddingBlock: 3,
-          borderRadius: 3,
-        }}
-        className="box-soft-shadow"
-      >
+				const accessToken = data.accessToken;
+				const refreshToken = data.refreshToken;
+				localStorage.setItem('accessToken', accessToken);
+				sessionStorage.setItem('refreshToken', refreshToken);
 
-        {
-          error &&
-          <Alert variant='outlined' severity='error' sx={{ m: 1 }}>Akun tidak valid.</Alert>
-        }
-        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-          <Lock />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="login"
-            label="Login"
-            name="login"
-            autoComplete="login"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-        </Box>
-      </Box>
-    </Container>
-  );
+				setAuth({ accessToken });
+				navigate(from, { replace: true });
+			}
+		};
+
+		fetchUser().catch(() => {
+			setError(true);
+		});
+	};
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<Box
+				sx={{
+					marginTop: 8,
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					paddingInline: 5,
+					paddingBlock: 3,
+					borderRadius: 3,
+				}}
+				className="box-soft-shadow"
+			>
+
+				{
+					error &&
+					<Alert variant='outlined' severity='error' sx={{ m: 1 }}>Akun tidak valid.</Alert>
+				}
+				<Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+					<Lock />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign in
+				</Typography>
+				<Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						id="login"
+						label="Login"
+						name="login"
+						autoComplete="login"
+						autoFocus
+					/>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+					/>
+					<FormControlLabel
+						control={<Checkbox value="remember" color="primary" />}
+						label="Remember me"
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Sign In
+					</Button>
+				</Box>
+			</Box>
+		</Container>
+	);
 };
 
-export default LogIn;
+export default Login;
