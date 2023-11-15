aeb8f3027082c85bc99c0f1379540df41a398cfe
AchLim <rimuakoru@gmail.com>
2023-11-15 23:43:16 +0700

    [REMOVE] Unused console log

--- src/utils/Converter.tsx
+++ src/utils/Converter.tsx
@@ -2,7 +2,5 @@
 
 export const ConvertDateTimeToDate = (datetime: Date) => {
     var date = datetime.toISOString().split('T')[0];
-    console.log('converted datetime to date: ' + date);
-    console.log('converted datetime to date: ' + new Date(date));
     return date;
 }
