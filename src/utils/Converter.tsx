4416c30bfaf5e5ef494c3ac37675a9cddcb51a5a
AchLim <rimuakoru@gmail.com>
2023-11-16 00:33:28 +0700

    [UPDATE] Add ConvertNumberToCurrency

--- src/utils/Converter.tsx
+++ src/utils/Converter.tsx
@@ -4,3 +4,7 @@ export const ConvertDateTimeToDate = (datetime: Date) => {
     var date = datetime.toISOString().split('T')[0];
     return date;
 }
+
+export const ConvertNumberToCurrency = (number: number) => {
+    return number.toLocaleString("id-ID", { style: "currency", currency: "IDR" });
+}
